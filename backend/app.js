1068099b932865754c2751b9f614bc7b4b38936d
ntmanua <manuarii.natanni@gmail.com>
2023-06-08 19:55:48 +0200

    fix server prblm

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-import * as data from 'data.json';
+const data = require('./data.json');
 
 
 app.use('/api/data', (req, res, next) => {
